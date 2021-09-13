577291d9b6c5552c76cff8b3020e5d98185faa25
Alexis Castro <alexiscastro-glz@outlook.com>
2021-09-13 06:13:21 +0000

    Files changed

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    validate();
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear() + parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit <span style='background-color: #FFFF00'>" + principal + "</span>,\<br\>at an interest rate of <span style='background-color: #FFFF00'>"+rate+"</span> %\<br\>You will receive an amount of <span style='background-color: #FFFF00'> "+interest+"</span>,\<br\>in the year <span style='background-color: #FFFF00'>"+year+" </span> \<br\>"
 }
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+function validate(){
+    var value = document.getElementById("principal").value;
+    if (value < 1){
+        alert("Enter a positive number");
+        value.focus();
+        return false;
+    }
+}
+
+
         
